e63b6f0742ab6583a50faccf07bcaedde90eea37
Mel <melcornish@me.com>
2021-09-10 14:44:42 +0100

    update hamburger timer

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ const list = document.querySelectorAll(".list-link");
 
 navToggle.addEventListener("click", () => {
     nav.classList.toggle("nav--visible");
+
+    setTimeout(function () {
+        document.querySelector('.nav-list').style.display = 'none'
+    }, 4000)
+    setTimeout("location.reload(true);", 4000);
 });
 
